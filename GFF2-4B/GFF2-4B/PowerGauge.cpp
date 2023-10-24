736720d232d3f1cdf6119a281b6f21543123d773
AtsuneSeragaki <120348226+AtsuneSeragaki@users.noreply.github.com>
2023-10-24 11:03:46 +0900

    強化ゲージ表示場所移動

--- GFF2-4B/GFF2-4B/PowerGauge.cpp
+++ GFF2-4B/GFF2-4B/PowerGauge.cpp
@@ -2,20 +2,20 @@
 
 PowerGauge::PowerGauge()
 {
-	magenta.x = 387.0f;
-	magenta.y = 68.0f;
+	magenta.x = 109.0f;
+	magenta.y = 64.0f;
 	magenta.volume = 0.0f;
 	magenta.maxFlg = 0;
 
-	cyan.x = 419.0f;
-	cyan.y = 114.0f;
-	cyan.volume = 0.0f;
-	cyan.maxFlg = 0;
-
-	yellow.x = 360.0f;
-	yellow.y = 114.0f;
+	yellow.x = 80.0f;
+	yellow.y = 109.0f;
 	yellow.volume = 0.0f;
 	yellow.maxFlg = 0;
+
+	cyan.x = 140.0f;
+	cyan.y = 109.0f;
+	cyan.volume = 0.0f;
+	cyan.maxFlg = 0;
 
 	image[0] = LoadGraph("resource/images/ã≠âªÉQÅ[ÉW1.png");
 	image[1] = LoadGraph("resource/images/ã≠âªÉQÅ[ÉW2.png");
@@ -32,11 +32,10 @@ void PowerGauge::Update()
 
 void PowerGauge::Draw() const
 {
-	
-	DrawGraph(285, 10, image[2], TRUE);
+	DrawGraph(5, 5, image[2], TRUE);
 	DrawBox((int)magenta.x - 50, (int)magenta.y - 45, (int)magenta.x, (int)magenta.y, 0xe4007f, TRUE);
+	DrawBox((int)cyan.x - 50, (int)cyan.y - 45, (int)cyan.x, (int)cyan.y, 0x00ffff, TRUE);
 	DrawBox((int)yellow.x - 50, (int)yellow.y - 45, (int)yellow.x, (int)yellow.y, 0xffff00, TRUE);
-	DrawBox((int)cyan.x - 50, (int)cyan.y - 45, (int)cyan.x, (int)cyan.y, 0x00FFFF, TRUE);
-	DrawGraph(300, 17, image[1], TRUE);
-	DrawGraph(285, 10, image[0], TRUE);
+	DrawGraph(20, 12, image[1], TRUE);
+	DrawGraph(5, 5, image[0], TRUE);
 }
