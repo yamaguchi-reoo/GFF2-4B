0832918e81ffc34728725a83e28dba90484effb3
AtsuneSeragaki <120348226+AtsuneSeragaki@users.noreply.github.com>
2023-11-02 14:43:33 +0900

    強化ゲージ修正

--- GFF2-4B/GFF2-4B/PowerGauge.cpp
+++ GFF2-4B/GFF2-4B/PowerGauge.cpp
@@ -6,6 +6,7 @@ PowerGauge::PowerGauge()
 	magenta.y = 70.0f;
 	magenta.h = 65.0f;
 	magenta.volume = 0.0f;
+	magenta.remainder = 0;
 	magenta.ratio = 0.0f;
 	magenta.maxFlg = 0;
 
@@ -13,6 +14,7 @@ PowerGauge::PowerGauge()
 	cyan.y = 130.0f;
 	cyan.h = 115.0f;
 	cyan.volume = 0.0f;
+	cyan.remainder = 0;
 	cyan.ratio = 0.0f;
 	cyan.maxFlg = 0;
 
@@ -20,6 +22,7 @@ PowerGauge::PowerGauge()
 	yellow.y = 150.0f;
 	yellow.h = 84.0f;
 	yellow.volume = 0.0f;
+	yellow.remainder = 0;
 	yellow.ratio = 0.0f;
 	yellow.maxFlg = 0;
 
@@ -27,13 +30,14 @@ PowerGauge::PowerGauge()
 	black.y = 138.0f;
 	black.h = 131.0f;
 	black.volume = 0.0f;
+	black.remainder = 0;
 	black.ratio = 0.0f;
 	black.maxFlg = 0;
 
 	powerFlg = 0;
 
-	image[0] = LoadGraph("resource/images/magatama_line.png");
-	image[1] = LoadGraph("resource/images/black_line.png");
+	image[0] = LoadGraph("resource/images/koban.png");
+	image[1] = LoadGraph("resource/images/black.png");
 	image[2] = LoadGraph("resource/images/magatama_max.png");
 	
 	MaskHandle[0] = LoadMask("resource/images/Magatama_mask1.png");
@@ -111,11 +115,16 @@ void PowerGauge::Draw() const
 #ifdef _DEBUG
 
 	//デバック表示
-	//DrawFormatString(300, 10, 0xffffff, "%f", num);
+	DrawFormatString(300, 10, 0xffffff, "%d", magenta.remainder);
+	DrawFormatString(300, 30, 0xffffff, "%f", magenta.volume);
+	DrawFormatString(300, 50, 0xffffff, "%d", cyan.remainder);
+	DrawFormatString(300, 70, 0xffffff, "%f", cyan.volume);
+	DrawFormatString(300, 90, 0xffffff, "%d", yellow.remainder);
+	DrawFormatString(300, 100, 0xffffff, "%f", yellow.volume);
+
 
 #endif // _DEBUG
 	
-
 	//マスク画面を作成
 	CreateMaskScreen();
 
@@ -127,7 +136,7 @@ void PowerGauge::Draw() const
 		//勾玉の背景を白に
 		DrawBox(5, 3, 155, 153, 0xffffff, TRUE);
 
-		//強化ゲージがMAXじゃないとき
+		//強化ゲージが0%以上の時に表示
 		if (yellow.volume != 0.0f)
 		{
 			DrawBoxAA(yellow.x - 92, yellow.y - yellow.ratio, yellow.x, yellow.y, 0xffff00, TRUE);
@@ -141,7 +150,7 @@ void PowerGauge::Draw() const
 			DrawBoxAA(magenta.x - 107, magenta.y - magenta.ratio, magenta.x, magenta.y, 0xe4007f, TRUE);
 		}
 		
-		//図形描画の重なりを隠す
+		/**図形描画の重なりを隠す(ここから)**/
 		DrawBox(87, 70, 112, 130, 0xffffff, TRUE);
 
 		if (cyan.volume >= 2.0f)
@@ -165,30 +174,31 @@ void PowerGauge::Draw() const
 		{
 			DrawBoxAA(80.0f, 70.0f - ((magenta.volume * 1.1f) / 100.0f * 59.5f ), 112.0f, 70.0f, 0xe4007f, TRUE);
 		}
+		/**図形描画の重なりを隠す(ここまで)**/
 	}
 	else if ((rotaFlg == 1) && (black.maxFlg == 0))
 	{
-		// 読みこんだグラフィックを回転描画
+		//強化ゲージを回転描画
 		DrawRotaGraph(80, 80, 1.0f, PI / 180 * num, image[2], TRUE, FALSE);
 	}
 	else if ((black.maxFlg == 1) && (powerFlg == 0))
-	{	
+	{//強化ゲージがMAX(黒)のとき
+
 		//ロードしたマスクデータを画面の左上に描画
 		DrawMask(5, 3, MaskHandle[1], DX_MASKTRANS_NONE);
 
 		DrawBox(5, 3, 155, 138, 0xffffff, TRUE);
-
-		//強化ゲージがMAXのとき
+		
 		DrawBoxAA(black.x - 135.0f, black.y - 131.0f, black.x, black.y, 0x000000, TRUE);
 	}
 	else if(powerFlg == 1)
-	{
+	{//強化ゲージがMAXでXボタンが押されたとき
+
 		//ロードしたマスクデータを画面の左上に描画
 		DrawMask(5, 3, MaskHandle[1], DX_MASKTRANS_NONE);
 
 		DrawBox(5, 3, 150, 138, 0xffffff, TRUE);
 
-		//強化ゲージがMAXでXボタンが押されたとき
 		DrawBoxAA(black.x - 135.0f, black.y - black.ratio, black.x, black.y, 0x000000, TRUE);
 	}
 
@@ -197,7 +207,7 @@ void PowerGauge::Draw() const
 }
 
 //ゲージの溜まり具合を計算
-float PowerGauge::VolumeCalculate(float volume, float h)
+float PowerGauge::CalculateVolume(float volume, float h)
 {
 	float i = 0.0f;
 
@@ -209,10 +219,10 @@ float PowerGauge::VolumeCalculate(float volume, float h)
 //ゲージの溜まり具合を計算した値を代入
 void PowerGauge::VolumeSet()
 {
-	magenta.ratio = VolumeCalculate(magenta.volume, magenta.h);
-	yellow.ratio = VolumeCalculate(yellow.volume, yellow.h);
-	cyan.ratio = VolumeCalculate(cyan.volume, cyan.h);
-	black.ratio = VolumeCalculate(black.volume, black.h);
+	magenta.ratio = CalculateVolume(magenta.volume, magenta.h);
+	yellow.ratio = CalculateVolume(yellow.volume, yellow.h);
+	cyan.ratio = CalculateVolume(cyan.volume, cyan.h);
+	black.ratio = CalculateVolume(black.volume, black.h);
 }
 
 //時間経過で黒のゲージを減らす
@@ -260,22 +270,34 @@ void PowerGauge::SetPowerFlg(int i)
 //敵が落とした色、量をゲージに加算
 void PowerGauge::SetVolume(ColorDate color)
 {
-			//マゼンタ
+	//マゼンタ
 	if (magenta.maxFlg == 0)
 	{
 		magenta.volume += color.magenta;
-
 	}
-			//シアン
+	else
+	{
+		magenta.remainder += (int)color.magenta;
+	}
+
+	//シアン
 	if (cyan.maxFlg == 0)
 	{
 		cyan.volume += color.syan;
 	}
-			//イエロー
+	else
+	{
+		cyan.remainder += (int)color.syan;
+	}
+	
+	//イエロー
 	if (yellow.maxFlg == 0)
 	{
 		yellow.volume += color.yellow;
-
+	}
+	else
+	{
+		yellow.remainder += (int)color.yellow;
 	}
 }
 
@@ -285,6 +307,7 @@ void PowerGauge::CheckVolumeMax()
 	//マゼンタを集めた量が100%以上だったらマゼンタのMAXフラグを1に
 	if (magenta.volume >= 100.0f)
 	{
+		magenta.remainder += (int)magenta.volume - 100;
 		magenta.volume = 100.0f;
 		magenta.maxFlg = 1;
 	}
@@ -292,6 +315,7 @@ void PowerGauge::CheckVolumeMax()
 	//イエローを集めた量が100%以上だったらイエローのMAXフラグを1に
 	if (yellow.volume >= 100.0f)
 	{
+		yellow.remainder += (int)yellow.volume - 100;
 		yellow.volume = 100.0f;
 		yellow.maxFlg = 1;
 	}
@@ -299,6 +323,7 @@ void PowerGauge::CheckVolumeMax()
 	//シアンを集めた量が100%以上だったらシアンのMAXフラグを1に
 	if (cyan.volume >= 100.0f)
 	{
+		cyan.remainder += (int)cyan.volume - 100;
 		cyan.volume = 100.0f;
 		cyan.maxFlg = 1;
 	}
