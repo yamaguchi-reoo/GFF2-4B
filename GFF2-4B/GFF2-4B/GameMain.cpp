63f66338418fc42317f18bacb802d098e483e3bb
kobayasihinata <120348245+kobayasihinata@users.noreply.github.com>
2023-11-08 13:10:56 +0900

    木と雲への乗り方調整

--- GFF2-4B/GFF2-4B/GameMain.cpp
+++ GFF2-4B/GFF2-4B/GameMain.cpp
@@ -542,7 +542,6 @@ void GameMain::LoadStageData(int _stage)
 }
 void GameMain::SetStage(int _stage)
 {
-	CameraLocation(screen_origin);
 	//敵と攻撃をリセット
 	for (int i = 0; i < ZAKURO_MAX; i++) {
 		zakuro[i] = nullptr;
@@ -620,6 +619,8 @@ void GameMain::SetStage(int _stage)
 	//プレイヤーのリスポーン
 	Location res_location = { 100,100 };
 	player->Respawn(res_location);
+	//カメラのリセット
+	ResetCamera();
 }
 void GameMain::CameraLocation(Location _location)
 {
