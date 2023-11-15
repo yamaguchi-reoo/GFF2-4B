7a5422b973baa5524dfdca06ba65a8814b0d5a29
git961 <120348157+git961@users.noreply.github.com>
2023-11-15 10:52:42 +0900

    削除

--- GFF2-4B/GFF2-4B/Boss.cpp
+++ GFF2-4B/GFF2-4B/Boss.cpp
@@ -4,7 +4,6 @@ Boss::Boss() {
 	LoadDivGraph("resource/images/Boss/BossBody.png", 3, 3, 1, 360, 700,Boss_MainBody);
 	LoadDivGraph("resource/images/Boss/BossArm.png", 2, 2, 1, 420, 700,Boss_MainArm);
 
-	test[0] = LoadGraph("resource/images/Boss/Himawari.png", true);
 
 
 	Count_Death = 2;
@@ -24,7 +23,7 @@ void Boss::Update(GameMain* main) {
 void Boss::Draw() const {
 
 	DrawGraph(440, 0, Boss_MainBody[0], TRUE);
-	DrawGraph(500, 600, test[0], TRUE);
+	DrawGraph(100, 0, Boss_MainArm[0], TRUE);
 	DrawGraph(800, 0, Boss_MainArm[1], TRUE);
 	DrawFormatString(400, 40, 0xff00ff, "death%d", Count_Death);
 
