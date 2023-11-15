3c1c8d46df6118b4749169a73a5821c8d5372e39
git961 <120348157+git961@users.noreply.github.com>
2023-11-16 08:56:14 +0900

    画像追加

--- GFF2-4B/GFF2-4B/Boss.cpp
+++ GFF2-4B/GFF2-4B/Boss.cpp
@@ -3,6 +3,7 @@
 Boss::Boss() {
 	LoadDivGraph("resource/images/Boss/BossBody.png", 3, 3, 1, 360, 700,Boss_MainBody);
 	LoadDivGraph("resource/images/Boss/BossArm.png", 2, 2, 1, 420, 700,Boss_MainArm);
+	LoadDivGraph("resource/images/Boss/explosionMini.png", 2, 2, 1, 160, 160, Explosion);
 
 
 
@@ -23,6 +24,9 @@ void Boss::Update(GameMain* main) {
 void Boss::Draw() const {
 
 	DrawGraph(440, 0, Boss_MainBody[0], TRUE);
+	DrawGraph(380, 0, Explosion[0], TRUE);
+	DrawGraph(380, 0, Explosion[1], TRUE);
+	//DrawGraph(440, 0, Boss_MainBody[0], TRUE);
 	DrawGraph(100, 0, Boss_MainArm[0], TRUE);
 	DrawGraph(800, 0, Boss_MainArm[1], TRUE);
 	DrawFormatString(400, 40, 0xff00ff, "death%d", Count_Death);
