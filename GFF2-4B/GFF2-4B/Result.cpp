2de7f2eaeaacd58536fe684c68d40a0a6409109f
AtsuneSeragaki <120348226+AtsuneSeragaki@users.noreply.github.com>
2023-11-16 13:43:30 +0900

    リザルト画面追加

--- GFF2-4B/GFF2-4B/Result.cpp
+++ GFF2-4B/GFF2-4B/Result.cpp
@@ -7,8 +7,16 @@
 Result::Result()
 {
 	score = Score::GetScore();
+	img[0] = LoadGraph("resource/images/makimono.png");
+	img[1] = LoadGraph("resource/images/Enemy/zakuro.png");
+	img[2] = LoadGraph("resource/images/Enemy/himawari.png");
+	img[3] = LoadGraph("resource/images/Enemy/iruka.png");
+	img[4] = LoadGraph("resource/images/Result1.png");
+	img[5] = LoadGraph("resource/images/Result2.png");
+
+
 	draw_score = 0;
-	LoadDivGraph("resource/images/ScoreNum.png", 10, 4, 3, 32, 32, num_img);
+	LoadDivGraph("resource/images/ResultNum.png", 10, 4, 3, 73, 73, num_img);
 }
 
 //ƒfƒXƒgƒ‰ƒNƒ^
@@ -53,6 +61,40 @@ AbstractScene* Result::Update()
 //•`‰æ‚ÉŠÖ‚·‚é‚±‚Æ‚ðŽÀ‘•
 void Result::Draw() const
 {
-	SetFontSize(60);
-	DrawFormatString(440, 290, 0xffffff, "“¾“_F%06d",draw_score);
+
+
+	DrawBox(0, 0, 1280, 720, 0xdddddd, TRUE);
+
+	DrawGraph(97, 30, img[0], TRUE);
+
+	//SetFontSize(30);
+	//DrawFormatString(600, 150, 0x000000, "¡‰ñ‚Ì¬‰Ê");
+	DrawGraph(535, 140, img[4], TRUE);
+
+	DrawGraph(370, 250, img[2], TRUE);
+	DrawFormatString(470, 295, 0x000000, "~");
+	DrawGraph(500, 280, num_img[0], TRUE);
+
+	DrawGraph(595, 300, img[3], TRUE);
+	DrawFormatString(705, 295, 0x000000, "~");
+	DrawGraph(735, 280, num_img[0], TRUE);
+
+	DrawGraph(855, 290, img[1], TRUE);
+	DrawFormatString(940, 295, 0x000000, "~");
+	DrawGraph(970, 280, num_img[0], TRUE);
+
+	DrawGraph(465, 455, img[5], TRUE);
+
+	//int a = draw_score;
+	int a = 999999;
+
+	int pos_x = 830;
+
+	do
+	{
+		DrawGraph(pos_x, 440, num_img[a % 10], TRUE);
+		a /= 10;
+		pos_x -= 40;
+	} while (a > 0);
+
 }
