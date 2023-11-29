a4f87704f16717bc236728ceb400e7648a5a47a2
れお <120348353+yamaguchi-reoo@users.noreply.github.com>
2023-11-29 14:32:18 +0900

    irukaアニメーション追加

--- GFF2-4B/GFF2-4B/GameMain.cpp
+++ GFF2-4B/GFF2-4B/GameMain.cpp
@@ -636,6 +636,14 @@ void GameMain::Draw() const
 		//描画
 	player->Draw();
 
+	// イルカ
+	for (int i = 0; i < IRUKA_MAX; i++)
+	{
+		if (iruka[i] != nullptr)
+		{
+			iruka[i]->Draw();
+		}
+	}
 	for (int i = 0; i < stage_height_num; i++)
 	{
 		for (int j = 0; j < stage_width_num; j++)
@@ -658,14 +666,6 @@ void GameMain::Draw() const
 		if (himawari[i] != nullptr)
 		{
 			himawari[i]->Draw();
-		}
-	}
-	// イルカ
-	for (int i = 0; i < IRUKA_MAX; i++)
-	{
-		if (iruka[i] != nullptr)
-		{
-			iruka[i]->Draw();
 		}
 	}
 	//竹
